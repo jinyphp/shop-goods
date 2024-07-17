2f9979bb8d3be8c8723190acb7f65668b40eaac1
LTSGOD <lts0424@naver.com>
2024-07-17 18:19:49 +0900

    feat: AdminReviewController와 사용자 화면 리뷰 연결
    - reviews 테이블에서 데이터 가져와 livewire로 blade 반환

--- src/Http/Livewire/ShopProductReview.php
+++ src/Http/Livewire/ShopProductReview.php
@@ -4,6 +4,7 @@ namespace Jiny\Shop\Goods\Http\Livewire;
 use Illuminate\Support\Facades\Blade;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 use Webuni\FrontMatter\FrontMatter;
 use Jiny\Pages\Http\Parsedown;
 use \Jiny\Html\CTag;
@@ -13,16 +14,27 @@ use \Jiny\Html\CTag;
 class ShopProductReview extends Component
 {
     public $likeCount;
+    public $slug;
+    public $productId;
 
-    public function mount() {
+    public function mount($product) {
         $this->likeCount = 0;
+        $this->productId = $product['id'];
+        // dd($product['id']);
     }
 
     public function render()
     {
+        $rows = DB::table('reviews')->where('order_item_id',$this->productId)->get();
+        // dump($rows);
 
-        // dd($rows);
-        return view('jiny-shop-goods::detail.productReview');
+        // 배열로 변환
+        $goods = $rows->map(function($row){
+            return (array) $row;
+        })->toArray();
+        // dd($goods);
+
+        return view('jiny-shop-goods::detail.productReview', ['rows'=>$goods]);
     }
 
     public function increaseLike(){
